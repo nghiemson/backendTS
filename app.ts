2e18fb71d69fc9b5cfa4db33a819a8182163c876
sonnghiem <sonnghiemaz@gmail.com>
2024-02-02 00:16:32 +0700

    config swagger

--- app.ts
+++ app.ts
@@ -1,10 +1,37 @@
 import dotenv from "dotenv";
 dotenv.config()
 
-import express from "express"
+import express, { json, urlencoded } from "express"
 import { connectToDb } from "./db/connect";
 
 const app = express();
+
+// middleware for json parsing of request body
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+// swagger UI
+import * as swaggerUI from "swagger-ui-express";
+import * as swaggerJson from "./tsoa/tsoa.json";
+
+// serve Swagger UI
+app.use(
+    ["/openapi", "/docs", "/swagger"],
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJson)
+  );
+  
+  // serve swagger JSON
+  app.get("/swagger.json", (_, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.sendFile(__dirname + "/tsoa/tsoa.json");
+  });
+
+// tsoa routes
+import { RegisterRoutes } from "./routes/routes";
+RegisterRoutes(app);
+
 const port = process.env.PORT || 3000;
 
 const start = async () => {
--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -1,23 +1,44 @@
 import { StatusCodes } from "http-status-codes";
 
-import { Body, Controller, OperationId, Post, Route, Tags } from "tsoa";
+import {
+  Body,
+  Controller,
+  OperationId,
+  Post,
+  Route,
+  Tags,
+  Security,
+} from "tsoa";
 
 import {
-    UserAndCredentials,
-    UserCreationParams,
-} from "../services/models/auth-models"
+  UserAndCredentials,
+  UserCreationParams,
+} from "../services/models/auth-models";
 
-import AuthService from "services/auth-services";
+import AuthService from "../services/auth-services";
 
 @Route("/api/v1/auth")
 @Tags("Auth")
 export class AuthController extends Controller {
-    @Post("register")
-    @OperationId("registerUser")
-    public async register(
-        @Body() requestBody: UserCreationParams
-    ): Promise<UserAndCredentials> {
-        this.setStatus(StatusCodes.CREATED);
-        return new AuthService().register(requestBody);
-    }
+  @Post("register")
+  @OperationId("registerUser")
+  public async register(
+    @Body() requestBody: UserCreationParams
+  ): Promise<UserAndCredentials> {
+    this.setStatus(StatusCodes.CREATED);
+    return new AuthService().register(requestBody);
+  }
+
+
+  // TODO: remove this dummy endpoint later when
+  // we have proper endpoints that use our
+  // authentication mechanism
+  @Post("dummy")
+  @OperationId("dummy")
+  @Security("jwt")
+  public async dummy(): Promise<void> {
+    this.setStatus(StatusCodes.OK);
+    return Promise.resolve();
+  }
 }
+
